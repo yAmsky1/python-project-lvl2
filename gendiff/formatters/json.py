7147c8ffa2bdedbd653dcf652723e3800b5834d0
Dmitri Spirin <yamsky.xbox@gmail.com>
2022-06-11 03:56:13 +0500

    Update tests update json formatter

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -2,4 +2,4 @@ import json
 
 
 def format_to_json(diff):
-    return json.dumps(diff)
+    return json.dumps(diff, sort_keys=True, indent=2)
--- tests/test_gen_diff.py
+++ tests/test_gen_diff.py
@@ -29,10 +29,10 @@ def result_plain():
         return res_file.read()
 
 
-# @pytest.fixture()
-# def result_json():
-#     with open('./tests/fixtures/result_json.txt') as res_file:
-#         return res_file.read()
+@pytest.fixture()
+def result_json():
+    with open('./tests/fixtures/result_json.txt') as res_file:
+        return res_file.read()
 
 
 def test_gen_diff_stylish(paths_json, paths_yaml, paths_mix, result_stylish):
@@ -47,7 +47,7 @@ def test_gen_diff_plain(paths_json, paths_yaml, paths_mix, result_plain):
     assert generate_diff(*paths_mix, form='plain') == result_plain
 
 
-# def test_gen_diff_json(paths_json, paths_yaml, paths_mix, result_json):
-#     assert generate_diff(*paths_json, form='json') == result_json
-#     assert generate_diff(*paths_yaml, form='json') == result_json
-#     assert generate_diff(*paths_mix, form='json') == result_json
+def test_gen_diff_json(paths_json, paths_yaml, paths_mix, result_json):
+    assert generate_diff(*paths_json, form='json') == result_json
+    assert generate_diff(*paths_yaml, form='json') == result_json
+    assert generate_diff(*paths_mix, form='json') == result_json
